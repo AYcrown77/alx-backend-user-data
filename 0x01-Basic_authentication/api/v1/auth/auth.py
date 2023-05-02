286a402b7557e329e1d39e2b22cfd9a57cc29963
AYcrown77 <aycrown77@gmail.com>
2023-05-02 13:27:01 +0100

    Solve task 3

---
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""Authentication module"""
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    """User authentication class"""
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Checks if the path requires authorization
+        """
+        if path is None or not excluded_paths:
+            return True
+        for ex_path in excluded_paths:
+            if ex_path.endswith('*') and path.startswith(ex_path[:-1]):
+                return False
+            elif ex_path in {path, path + '/'}:
+                return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Checks for  Authorization key in request header
+        """
+        if request is None:
+            return None
+        if request.headers.get('Authorization'):
+            return request.headers.get('Authorization')
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Checks for current user
+        """
+        return None
