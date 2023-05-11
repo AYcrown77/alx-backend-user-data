4e26dad2217265100cd33b458260c5ce1ae9f49e
AYcrown77 <aycrown77@gmail.com>
2023-05-11 11:03:03 +0100

    Solve task 1

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,39 +1,35 @@
-"""
-Database  module
+"""DB module
 """
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import InvalidRequestError
-from sqlalchemy.orm.exc import NoResultFound
-from user import Base, User
+
+from user import Base
 
 
 class DB:
+    """DB class
     """
-    Database class
-    """
-     def __init__(self) -> None:
+
+    def __init__(self) -> None:
+        """Initialize a new DB instance
         """
-        Initializing  a new Database instance
-        """
-        self._engine = create_engine("sqlite:///a.db", echo=False)
+        self._engine = create_engine("sqlite:///a.db", echo=True)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
     def _session(self) -> Session:
-        """
-        Memoized session object of the database
+        """Memoized session object
         """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
-        return self.__session
+        return self.__session    
 
-     def add_user(self, email: str, hashed_password: str) -> User:
+    def add_user(self, email: str, hashed_password: str) -> User:
         """
         Adding a new user to database
         """
@@ -43,5 +39,4 @@ class DB:
             )
         self._session.add(new_usr)
         self._session.commit()
-
         return new_usr
