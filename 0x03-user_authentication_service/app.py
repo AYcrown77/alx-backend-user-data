b658d6f6917656c016a33b86f0b26926e0090488
AYcrown77 <aycrown77@gmail.com>
2023-05-11 13:14:32 +0100

    Solve task 11

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -28,3 +28,18 @@ def users():
             })
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login():
+    """Creates a new session for user"""
+    email = request.form["email"]
+    password = request.form["password"]
+
+    if not AUTH.valid_login(email, password):
+        abort(401)
+
+    session_id = AUTH.create_session(email)
+    resp = jsonify({"email": email, "message": "logged in"})
+    resp.set_cookie("session_id", session_id)
+    return resp
