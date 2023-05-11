e7d0bbb220780076984f19ca6d6b89ab07415a4f
AYcrown77 <aycrown77@gmail.com>
2023-05-11 11:46:20 +0100

    Solve task 6

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
-""" Flask Application """
+""" Flask Application module"""
 
 from auth import Auth
 from flask import Flask, jsonify, request, abort, redirect
+
+
+app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", methods=["GET"], strict_slashes=False)
