aec5bef51144cc2b022d34176b6bb00aa6da4e15
AYcrown77 <aycrown77@gmail.com>
2023-05-11 11:22:10 +0100

    Solve task 2

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -41,7 +41,7 @@ class DB:
         self._session.commit()
         return new_usr
 
-     def find_user_by(self, **kwargs) -> User:
+    def find_user_by(self, **kwargs) -> User:
         """
         Returns the first row found in the users table as filtered
         by the method’s input arguments
