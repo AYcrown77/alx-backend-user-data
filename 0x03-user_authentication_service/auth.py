50d8fd598d8e99b2a790524a9a53fd60dd5655b1
AYcrown77 <aycrown77@gmail.com>
2023-05-11 11:39:25 +0100

    Solve task 4

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -17,3 +17,19 @@ def _hash_password(password: str) -> bytes:
     hash_pwd = bcrypt.hashpw(byte_pwd, salt)
 
     return hash_pwd
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+     def register_user(self, email: str, password: str) -> User:
+        """Saves a user with email and password to DB"""
+        try:
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            hash_pwd = _hash_password(password)
+            return self._db.add_user(email=email, hashed_password=hash_pwd)
